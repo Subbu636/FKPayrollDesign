2e4cd3dc972a5ba094189703592284588000b967
Subhash <subhashsubbu636@gmail.com>
2020-05-10 23:51:34 +0530

    Feature06 - Corrected commission system, Added weekly dues Comments : Commonly written for both types of employees Todo : Adding Database to the Project

--- SourceCode/Main.java
+++ SourceCode/Main.java
@@ -3,6 +3,10 @@
 import java.util.*;
 import java.io.*;
 import payroll.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 // Feature testing file
 
@@ -11,10 +15,35 @@ public class Main{
 	public static void main(String[] args){
 
 		System.out.println("Starting");
+
+
+		// Connection conn = null;
+		// try {
+		//     // db parameters
+		//     String url       = "jdbc:mysql://localhost:3306/mysqljdbc";
+		//     String user      = "root";
+		//     String password  = "sorted";
+			
+		//     // create a connection to the database
+		//     conn = DriverManager.getConnection(url, user, password);
+		//     // more processing here
+		//     // ...	
+		// } catch(SQLException e) {
+		//    System.out.println(e.getMessage());
+		// } finally {
+		// 	try{
+		//            if(conn != null)
+		//              conn.close();
+		// 	}catch(SQLException ex){
+		//            System.out.println(ex.getMessage());
+		// 	}
+		// }
+
 		Payroll pr = new Payroll(0);
-		int id1 = pr.add_employee("emp1", "work by hour", true, "Account", 10);
-		int id2 = pr.add_employee("emp2", "flat salary", false, "Mail", 10000);
-		int id3 = pr.add_employee("emp3", "commission", false, "Mail", 30);
+		int id1 = pr.add_employee("emp1", "work by hour", true, "Account", 10,0,15);
+		int id2 = pr.add_employee("emp2", "flat salary", true, "Mail", 10000,50,100);
+		int id3 = pr.add_employee("emp3", "flat salary", false, "Hand", 6000,50,0);
+		int id4 = pr.add_employee("emp4", "work by hour", false, "Mail", 20,0,0);
 
 
 		// pr.de_bug(id1);
@@ -43,30 +72,26 @@ public class Main{
 
 
 
-		pr.add_service_charge(id1,25);
-		pr.add_service_charge(id3, 50);
-		pr.add_service_charge(id2, 100);
+		// pr.add_service_charge(id1,25);
+		// pr.add_service_charge(id3, 50);
+		// pr.add_service_charge(id2, 100);
+		// pr.add_service_charge(id4, 30);
 		for(int i = 1;i < 29;i++){
 			System.out.println("Day:"+i);
 			pr.post_time_card(id1,9);
-			pr.post_sales_reciept(id3, 10);
+			pr.post_time_card(id4,8);
+			pr.post_sales_reciept(id2, 5);
+			pr.post_sales_reciept(id3,10);
 
-			double var1 = pr.process_payroll(id1);
-			if (var1 != -1.0){
-				System.out.println(id1 +":"+var1);
-			}
-			double var2 = pr.process_payroll(id2);
-			if (var2 != -1.0){
-				System.out.println(id2 +":"+var2);
-			}
-			double var3 = pr.process_payroll(id3);
-			if (var3 != -1.0){
-				System.out.println(id3 +":"+var3);
-			}
+			HashMap<Integer,Double> map = pr.process_payroll_all();
+			// System.out.println(map);
+			pr.de_bug(id1);
 
 			pr.increment_day();
 
 		}
+
+
 
 
 	}
--- SourceCode/payroll/Payroll.java
+++ SourceCode/payroll/Payroll.java
@@ -5,27 +5,42 @@ package payroll;
 // imports
 import java.util.*;
 import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 class Employee{
 
-	int id, start_date;
+	int id, start_date, payment_frequency,sales_count,commission_frequency, last_collection;
 	String name;
 	boolean is_union_member;
 	String payment_mode;
-	double salary_per_unit, service_charges;
+	double salary_per_unit, service_charges, commission_rate, weekly_dues;
 
-	Employee(int id,String name,boolean is_union_member,String payment_mode,double salary_per_unit,int start_date){
+	Employee(int id,String name,boolean is_union_member,String payment_mode,double salary_per_unit,int start_date,double commission_rate, double weekly_dues){
 		this.id = id;
 		this.name = name;
 		this.is_union_member = is_union_member;
 		this.payment_mode = payment_mode;
 		this.salary_per_unit = salary_per_unit;
 		this.start_date = start_date;
+		this.commission_rate = commission_rate;
+		this.weekly_dues = weekly_dues;
+		last_collection = 0;
+		sales_count = 0;
 		service_charges = 0;
+		payment_frequency = -1;
+		commission_frequency = 14;
 	}
 
 	double payroll(int day_count){
 		return -1.0;
+	}
+
+	void pay(double amount){
+		if (amount != 0.0){
+			System.out.println(amount+" units salary paid by "+payment_mode+" to id:"+id);
+		}
 	}
 }
 
@@ -34,98 +49,79 @@ class Work_by_hour extends Employee{
 	int normal_hrs;
 	int extra_hrs;
 	double extra_hour_factor = 1.5;
-	int payment_frequency = 7;
 
-	Work_by_hour(int id,String name,boolean is_union_member,String payment_mode,double salary_per_unit,int start_date){
-		super(id, name, is_union_member, payment_mode, salary_per_unit, start_date);
+	Work_by_hour(int id,String name,boolean is_union_member,String payment_mode,double salary_per_unit,int start_date,double commission_rate,double weekly_dues){
+		super(id, name, is_union_member, payment_mode, salary_per_unit, start_date,commission_rate,weekly_dues);
 		normal_hrs = 0;
 		extra_hrs = 0;
+		payment_frequency = 7;
 	}
 
 	double payroll(int day_count){
 
-		double sum = -1.0;
-		if ((day_count - start_date + 1)%payment_frequency == 0){
-			sum = (double)normal_hrs*salary_per_unit + (double)extra_hrs*salary_per_unit*extra_hour_factor;
-			if (sum > service_charges){
-				sum -= (service_charges);
-				service_charges = 0;
-			}
-			else{
-				sum = 0.0;
-				service_charges = service_charges - sum;
-			}
-			normal_hrs = 0;
-			extra_hrs = 0;
-			// System.out.println("Payment processed:"+id);
+		double ret_sum = (double)normal_hrs*salary_per_unit + (double)extra_hrs*salary_per_unit*extra_hour_factor;
+		double ret_comm = (double)sales_count*commission_rate;
+		double sum = 0;
+		if(is_union_member){
+			int num_weeks = (day_count + 1 - last_collection)/7;
+			last_collection = day_count + 1- ((day_count + 1 - last_collection)%7);
+			service_charges += ((double)num_weeks*weekly_dues);
+		}
+		if ((day_count+1)%payment_frequency == 0){
+			sum += ret_sum;
+		}
+		if ((day_count+1)%commission_frequency == 0){
+			sum += ret_comm;
+			sales_count = 0;
+		}
+		if (sum > service_charges){
+			sum -= (service_charges);
+			service_charges = 0;
 		}
 		else{
-			// System.out.println(((day_count - start_date + 1)%payment_frequency) +" days till payment:"+id);
+			sum = 0.0;
+			service_charges = service_charges - sum;
 		}
-		return sum;
+		super.pay(sum);
+		return ret_sum + ret_comm - service_charges;
 	}
 }
 
 class Flat_salary extends Employee{
 
-	int payment_frequency = 28;
 
-	Flat_salary(int id,String name,boolean is_union_member,String payment_mode,double salary_per_unit,int start_date){
-		super(id, name, is_union_member, payment_mode, salary_per_unit, start_date);
+	Flat_salary(int id,String name,boolean is_union_member,String payment_mode,double salary_per_unit,int start_date,double commission_rate,double weekly_dues){
+		super(id, name, is_union_member, payment_mode, salary_per_unit, start_date,commission_rate,weekly_dues);
+		payment_frequency = 28;
 	}
 
 	double payroll(int day_count){
 
-		double sum = -1.0;
-		if ((day_count - start_date + 1)%payment_frequency == 0){
-			sum = salary_per_unit;
-			if (sum > service_charges){
-				sum -= (service_charges);
-				service_charges = 0;
-			}
-			else{
-				sum = 0.0;
-				service_charges = service_charges - sum;
-			}
-			// System.out.println("Payment processed:"+id);
+		double ret_sum = salary_per_unit;
+		double ret_comm = (double)sales_count*commission_rate;
+		double sum = 0;
+		if(is_union_member){
+			int num_weeks = (day_count + 1 - last_collection)/7;
+			last_collection = day_count + 1 - ((day_count + 1 - last_collection)%7);
+			service_charges += ((double)num_weeks*weekly_dues);
 		}
-		else{
-			// System.out.println(((day_count - start_date + 1)%payment_frequency) +" days till payment:"+id);
+		if ((day_count+1)%payment_frequency == 0){
+			sum += ret_sum;
 		}
-		return sum;
-	}
-}
-
-class Commission_payment extends Employee{
-
-	int payment_frequency = 14;
-	int sales_count;
-
-	Commission_payment(int id,String name,boolean is_union_member,String payment_mode,double salary_per_unit,int start_date){
-		super(id, name, is_union_member, payment_mode, salary_per_unit, start_date);
-		sales_count = 0;
-	}
-
-	double payroll(int day_count){
-
-		double sum = -1.0;
-		if ((day_count - start_date + 1)%payment_frequency == 0){
-			sum = (double)sales_count*salary_per_unit;
-			if (sum > service_charges){
-				sum -= (service_charges);
-				service_charges = 0;
-			}
-			else{
-				sum = 0.0;
-				service_charges = service_charges - sum;
-			}
+		if ((day_count+1)%commission_frequency == 0){
+			sum += ret_comm;
 			sales_count = 0;
-			// System.out.println("Payment processed:"+id);
+		}
+		if (sum > service_charges){
+			sum -= (service_charges);
+			service_charges = 0;
 		}
 		else{
-			// System.out.println(((day_count - start_date + 1)%payment_frequency) +" days till payment:"+id);
+			sum = 0.0;
+			service_charges = service_charges - sum;
 		}
-		return sum;
+		super.pay(sum);
+		return ret_sum + ret_comm - service_charges;
 	}
 }
 
@@ -148,24 +144,18 @@ public class Payroll{
 		day_count += 1;
 	}
 
-	public int add_employee(String name,String salary_type,boolean is_union_member,String payment_mode,double salary_per_unit){
+	public int add_employee(String name,String salary_type,boolean is_union_member,String payment_mode,double salary_per_unit,double commission_rate,double weekly_dues){
 
 		avail_id += 1;
 		int id = avail_id;
 		if (salary_type.equals("work by hour")){
-			Employee emp = new Work_by_hour(id, name, is_union_member, payment_mode, salary_per_unit,day_count);
+			Employee emp = new Work_by_hour(id, name, is_union_member, payment_mode, salary_per_unit,day_count,commission_rate,weekly_dues);
 			employees.put(id, emp);
 			type_matching.put(id, salary_type);
 			// System.out.println("Employee "+name+" successfully added");
 		}
 		else if(salary_type.equals("flat salary")){
-			Employee emp = new Flat_salary(id, name, is_union_member, payment_mode, salary_per_unit,day_count);
-			employees.put(id, emp);
-			type_matching.put(id, salary_type);
-			// System.out.println("Employee "+name+" successfully added");
-		}
-		else if(salary_type.equals("commission")){
-			Employee emp = new Commission_payment(id, name, is_union_member, payment_mode, salary_per_unit,day_count);
+			Employee emp = new Flat_salary(id, name, is_union_member, payment_mode, salary_per_unit,day_count,commission_rate,weekly_dues);
 			employees.put(id, emp);
 			type_matching.put(id, salary_type);
 			// System.out.println("Employee "+name+" successfully added");
@@ -217,8 +207,8 @@ public class Payroll{
 
 	public int post_sales_reciept(int id, int sales_count){
 
-		if (employees.containsKey(id) && type_matching.get(id).equals("commission")){
-			Commission_payment emp = (Commission_payment)employees.get(id);
+		if (employees.containsKey(id)){
+			Employee emp = employees.get(id);
 			emp.sales_count += sales_count;
 			// System.out.println("Sales reciept added successfully:"+id);
 			return emp.sales_count;
@@ -288,6 +278,20 @@ public class Payroll{
 		}
 	}
 
+	public int alter_commission_rate(int id, int new_rate){
+
+		if(employees.containsKey(id)){
+			Employee emp = employees.get(id);
+			emp.commission_rate = new_rate;
+			// System.out.println("Changed payment mode:"+id);
+			return 1;
+		}
+		else{
+			// System.out.println("invalid id:"+id);
+			return -1;
+		}
+	}
+
 	public double process_payroll(int id){
 
 		if (employees.containsKey(id)){
@@ -301,20 +305,21 @@ public class Payroll{
 	}
 
 	public HashMap<Integer,Double> process_payroll_all(){
-		HashMap <Integer,Double> pay = new HashMap<>();
+		HashMap <Integer,Double> pay_table = new HashMap<>();
 		for (Map.Entry val : employees.entrySet()){
 			Employee emp = (Employee)val.getValue();
-			pay.put((Integer)val.getKey(),emp.payroll(day_count));
+			pay_table.put((Integer)val.getKey(),emp.payroll(day_count));
 		}
-		return pay;
+		return pay_table;
 	}
 
-	public void de_bug(int id){ // debugggggggggging
+	public void de_bug(int id){
 
 		if (employees.containsKey(id)){
 			Employee emp = employees.get(id);
-			System.out.println(emp.payment_mode);
-			System.out.println(emp.is_union_member);
+			// System.out.println(emp.name);
+			System.out.println(emp.service_charges);
+			System.out.println(emp.last_collection);
 		}
 		else{
 			System.out.println("No Such id");
